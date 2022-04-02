09abb2bea15f9a5aa083c7c92227694a474a107f
Kayode Odole <odoleemma@gmail.com>
2022-04-02 10:07:35 +0100

    fixed error with skaffold unable to start auth

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,4 +4,4 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.listen(3000, () => console.log('auth listening on port 3000 '));
+app.listen(3000, () => console.log('auth listening on port 3000!'));
